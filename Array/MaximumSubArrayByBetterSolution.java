d5de2a6e6a48275abc21c7f4cf2327b621ed947c
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2025-09-03 23:57:36 +0530

    Add files via upload

---
+++ Array/MaximumSubArrayByBetterSolution.java
@@ -0,0 +1,21 @@
+public class MaximumSubArrayByBetterSolution {
+    public static int maxSubarraySum(int[] arr, int n) {
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                maxi = Math.max(maxi, sum);
+            }
+
+        }
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int n = arr.length;
+        int maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+    }
+}
