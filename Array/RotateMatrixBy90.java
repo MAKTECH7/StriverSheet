6f7323befe2bc71ea864f6519941b8b064d9e042
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2025-09-24 16:37:31 +0530

    Add files via upload

---
+++ Array/RotateMatrixBy90.java
@@ -0,0 +1,27 @@
+public class RotateMatrixBy90 {
+    public static int[][] RotateByNintyDegree(int[][] matrix, int n) {
+        int[][] rotated = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[n - 1 - i][j] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+        };
+        int n = arr.length;
+        int[][] ans = RotateByNintyDegree(arr, n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
