5df441f4273c352bdf0b266dc8c97a21541d68c9
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2025-10-25 02:22:43 +0530

    Add files via upload

---
+++ String/LongestPrefix.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class LongestPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                return ans.toString();
+            }
+            ans.append(first.charAt(i));
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = { "flower", "flow", "flight" };
+
+        String str1 = longestCommonPrefix(strs);
+        System.out.println("The longest common prefix : " + str1);
+    }
+}
